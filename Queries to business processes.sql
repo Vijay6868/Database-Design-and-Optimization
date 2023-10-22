07e08aebfad6cecceed2b12a06585c7a2f5179d8
Vijay6868 <saroyav3@gmail.com>
2023-10-22 13:56:57 +1300

    finished queries till 6 th buisness process

--- Queries to business processes.sql
+++ Queries to business processes.sql
@@ -69,45 +69,130 @@ ORDER BY DESCRIPTION;
 SELECT * FROM SKILL
 ORDER BY DESCRIPTION;
 --List skills (skill id, description, and years) by description for a selected vacancy.
-
+SELECT 
+    VS.SKILLID, 
+    S.DESCRIPTION, 
+    VS.YEARS 
+FROM 
+    VACANCYSKILL VS
+INNER JOIN SKILL S ON S.SKILLID = VS.SKILLID
+WHERE VS.VACANCYID = 16
+ORDER BY S.DESCRIPTION;
 --List qualifications (qualification id, level and description) by description.
+SELECT * FROM qualification
+ORDER BY DESCRIPTION;
 --List qualifications (qualification id, level and description) by description for a selected vacancy.
+SELECT
+    VQ.QUALIFICATIONID,
+    Q.QUAL_LEVEL,
+    Q.DESCRIPTION
+FROM 
+    VACANCYQUALIFICATION VQ
+INNER JOIN QUALIFICATION Q ON Q.QUALIFICATIONID = VQ.QUALIFICATIONID
+WHERE VQ.VACANCYID = 2
+ORDER BY DESCRIPTION;
 
-5.	Apply a candidate to a vacancy.
+--5.	Apply a candidate to a vacancy.
 
-•	List vacancy details (vacancy ID, description, type, and employer name) for unfilled vacancies only by description.
-•	List applications (candidate’s last name and first name) for a selected vacancy by candidate’s last name
-•	List candidate details (candidate ID, last name, and first name) by first name within last name.
-•	List skills (skill id, description, and years) by description for a selected vacancy.
-•	List qualifications (qualification id, level and description) by description for a selected vacancy.
-•	List skills (skill id, description, and years) by description for a selected candidate.
-•	List qualifications (qualification id, level and description) by description for a selected candidate.
- 
-6.	Mark a vacancy as filled.
+--List vacancy details (vacancy ID, description, type, and employer name) for unfilled vacancies only by description.
+SELECT
+    V.VACANCYID,
+    V.DESCRIPTION,
+    E.EMPLOYERNAME
+FROM
+    VACANCY V
+INNER JOIN EMPLOYER E ON E.EMPLOYERID = V.EMPLOYERID
+WHERE STATUS = 'Unfilled'
+ORDER BY DESCRIPTION;
 
-•	List vacancy details (vacancy ID, description, type, hourly rate and employer name) for unfilled vacancies only by description.
-•	List applications (status, candidate’s last name and first name) for a selected vacancy by candidate’s last name
+--List applications (candidate’s last name and first name) for a selected vacancy by candidate’s last name
+SELECT
+    C.LASTNAME,
+    C.FIRSTNAME
+FROM CANDIDATE C
+INNER JOIN APPLICATION A ON C.CANDIDATEID = A.CANDIDATEID
+WHERE A.VACANCYID = 8
+ORDER BY C.LASTNAME;
 
-7.	All filled vacancies.
+--List candidate details (candidate ID, last name, and first name) by first name within last name.
+SELECT CANDIDATEID, LASTNAME, FIRSTNAME FROM CANDIDATE
+ORDER BY LASTNAME, FIRSTNAME;
 
-•	For each filled vacancy list the vacancy id, description, type, status, category name, employer name, employer street address, employer suburb, descriptions of skills, and descriptions of qualifications.
+--List skills (skill id, description, and years) by description for a selected vacancy.
+SELECT 
+    VS.SKILLID, 
+    S.DESCRIPTION, 
+    VS.YEARS 
+FROM 
+    VACANCYSKILL VS
+INNER JOIN SKILL S ON S.SKILLID = VS.SKILLID
+WHERE VS.VACANCYID = 16
+ORDER BY S.DESCRIPTION;
+--List qualifications (qualification id, level and description) by description for a selected vacancy.
+SELECT
+    VQ.QUALIFICATIONID,
+    Q.QUAL_LEVEL,
+    Q.DESCRIPTION
+FROM 
+    VACANCYQUALIFICATION VQ
+INNER JOIN QUALIFICATION Q ON Q.QUALIFICATIONID = VQ.QUALIFICATIONID
+WHERE VQ.VACANCYID = 2
+ORDER BY DESCRIPTION;
 
-8.	All unfilled vacancies.
+--List skills (skill id, description, and years) by description for a selected candidate.
+SELECT CS.SKILLID, S.DESCRIPTION, CS.YEARS FROM CANDIDATESKILL CS
+INNER JOIN SKILL S ON CS.SKILLID=S.SKILLID
+WHERE CANDIDATEID = 2 
+ORDER BY DESCRIPTION;
 
-•	For each unfilled vacancy list the vacancy id, description, type, status, category name, employer name, employer street address, employer suburb, descriptions of skills, and descriptions of qualifications.
+--List qualifications (qualification id, level and description) by description for a selected candidate.
+SELECT CQ.QUALIFICATIONID, Q.QUAL_LEVEL, Q.DESCRIPTION FROM CANDIDATEQUALIFICATION CQ
+INNER JOIN QUALIFICATION Q ON Q.QUALIFICATIONID = CQ.QUALIFICATIONID
+WHERE CQ.CANDIDATEID = 4;
 
-9.	All unfilled vacancies with candidates interested.
+--6.	Mark a vacancy as filled.
 
-•	For each unfilled vacancy list the vacancy id, description, type, status, employer name, descriptions and years of skills, descriptions of qualifications and full names of the candidates.
+--List vacancy details (vacancy ID, description, type, hourly rate and employer name) for unfilled vacancies only by description.
+SELECT
+    V.VACANCYID,
+    V.DESCRIPTION,
+    V.TYPE,
+    V.STATUS,
+    V.HOURLYRATE,
+    E.EMPLOYERNAME
+FROM
+    VACANCY V
+INNER JOIN EMPLOYER E ON E.EMPLOYERID = V.EMPLOYERID
+WHERE STATUS = 'Unfilled'
+ORDER BY DESCRIPTION;
+--List applications (status, candidate’s last name and first name) for a selected vacancy by candidate’s last name
+SELECT
+    C.LASTNAME,
+    C.FIRSTNAME
+FROM CANDIDATE C
+INNER JOIN APPLICATION A ON C.CANDIDATEID = A.CANDIDATEID
+WHERE A.VACANCYID = 8
+ORDER BY C.LASTNAME;
+--7.	All filled vacancies.
 
-10.	Vacancies by Category.
+--For each filled vacancy list the vacancy id, description, type, status, category name, employer name, employer street address, employer suburb, descriptions of skills, and descriptions of qualifications.
 
-•	For each vacancy list the vacancy id, description, hourly rate, and category name. Sort by category name.
+--8.	All unfilled vacancies.
 
-11.	Vacancies by Employer.
+--For each unfilled vacancy list the vacancy id, description, type, status, category name, employer name, employer street address, employer suburb, descriptions of skills, and descriptions of qualifications.
 
-•	For each vacancy list the vacancy id, type, status, and employer name. Sort by employer name.
+--9.	All unfilled vacancies with candidates interested.
 
-12.	All Candidates.
+--For each unfilled vacancy list the vacancy id, description, type, status, employer name, descriptions and years of skills, descriptions of qualifications and full names of the candidates.
 
-•	For each candidate list the candidate id, last name, first name, street address, suburb, phone number, status, and certification date (if applicable). Sort by first name within last name.
+--10.	Vacancies by Category.
+
+--For each vacancy list the vacancy id, description, hourly rate, and category name. Sort by category name.
+
+--11.	Vacancies by Employer.
+
+--For each vacancy list the vacancy id, type, status, and employer name. Sort by employer name.
+
+--12.	All Candidates.
+
+--For each candidate list the candidate id, last name, first name, street address, suburb, phone number, status, and certification date (if applicable). Sort by first name within last name.
